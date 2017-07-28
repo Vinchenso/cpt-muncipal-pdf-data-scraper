c1fd97fd71540644799ecafb17d3aea9bf5622bc
Vinchenso <chensok@gmail.com>
2017-07-28 17:39:53 +0200

    edit output to know beginnging and end of page

--- reader.rb
+++ reader.rb
@@ -7,10 +7,11 @@ file = File.open('./pdf_examples/fake_example.pdf', "rb")
 
 def print_header(page, end_of_header_trigger, end_of_summary_trigger)
   page_text = StringScanner.new(page.text)
+  puts '--------------------Begining of Page------------------------'
   puts page_text.scan_until(end_of_header_trigger)
   puts '------------------------------------------------------------'
   puts page_text.scan_until(end_of_summary_trigger)
-  puts '------------------------------------------------------------'
+  puts '--------------------End of Page-----------------------------'
 end
 
 PDF::Reader.open(file) do |reader|
