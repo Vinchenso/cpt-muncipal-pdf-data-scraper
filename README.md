238c91b3a7a66e330adb1c4ec14654e0167b38b2
Vinchenso <chensok@gmail.com>
2017-07-28 15:45:00 +0200

    add pry and scanned front page(header+Summary)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cpt-muncipal-pdf-data-scraper
 Automates the gathering of all data from Cape Town Municpal PDFs
+
+##Based on the following tutorials:
+
+- [Scraping PDF Files using Ruby](http://www.thewanderingconsultant.com/scraping-pdf-files-using-ruby/)
+
--- reader.rb
+++ reader.rb
@@ -1,11 +1,23 @@
 require 'pdf/reader'
 require 'rubygems'
+require 'strscan'
+require 'pry'
 
 file = File.open('./pdf_examples/fake_example.pdf', "rb")
 
-PDF::Reader.open(file) do |reader| 
+def print_header(page, end_of_header_trigger, end_of_summary_trigger)
+  page_text = StringScanner.new(page.text)
+  puts page_text.scan_until(end_of_header_trigger)
+  puts '------------------------------------------------------------'
+  puts page_text.scan_until(end_of_summary_trigger)
+  puts '------------------------------------------------------------'
+end
+
+PDF::Reader.open(file) do |reader|
   reader.pages.each do |page|
-    puts page.text
+    print_header(page, /Account Summary/i, /Please note/i) 
   end
 end
-                 
+
+
+
